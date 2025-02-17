bf502f2d32e46314e6db571cb23a4499445e668e
UihyunLee-93 <uihyun.dev@gmail.com>
2025-02-17 12:36:43 +0900

    -init

---
+++ LearnRunApp/LearnRunApp/LearnRunAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  LearnRunAppApp.swift
+//  LearnRunApp
+//
+//  Created by Uihyun.Lee on 2/17/25.
+//
+
+import SwiftUI
+
+@main
+struct LearnRunAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
